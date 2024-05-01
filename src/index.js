d760205ee740f3439c7f2ed2d10c6ac671eec4aa
jrbarron22 <72619274+jrbarron22@users.noreply.github.com>
2024-05-01 14:50:05 -0500

    Fixed require statement in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const parser = require('../exampletests/parser.js');
-const song = require('./src/music/song.js');
+const song = require('./music/song.js');
 
 module.exports = {parser, song};
